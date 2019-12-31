bbe460cf74edcfd4338b9c548b47fcad4ffb7f32
haley <haleyl@foxmail.com>
2019-12-31 19:45:54 +0800

    make build

--- internal/controllers/api/deposit.go
+++ internal/controllers/api/deposit.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/haleyrom/wallet/core"
 	"github.com/haleyrom/wallet/internal/models"
@@ -248,7 +249,9 @@ func TopUpDeposit(c *gin.Context) {
 
 	var confirm bool
 	// 判断是否已经存在订单
-	if err := detail.IsTransactionHash(o); err != nil {
+	err := detail.IsTransactionHash(o)
+	fmt.Println(detail,"=======")
+	if err != nil {
 		// 是否到确认数量
 		if coin.ConfirmCount <= block_count {
 			confirm = true
